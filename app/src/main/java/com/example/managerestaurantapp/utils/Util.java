4b6cf1208ee8c532a157e0b6f600b2eb0600a686
hohoanghuy13 <hohoanghuy170@gmail.com>
2024-06-07 21:51:26 +0700

    Merge project Quoc Cong processing

---
+++ app/src/main/java/com/example/managerestaurantapp/utils/Util.java
@@ -0,0 +1,9 @@
+package com.example.managerestaurantapp.utils;
+
+import com.example.managerestaurantapp.models.User;
+
+public class Util {
+    public static final String BASE_URL2 = "http://192.168.0.66:2109/dangnhap/";
+    public static final String BASE_URL = "http://192.168.1.8/Cong/";
+    public static User user_current = new User();
+}
